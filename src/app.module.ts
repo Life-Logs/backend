13f6686b845d7dbf0fd440199e9a54ab130445ad
Intra id : tkim <39645477+kth2624@users.noreply.github.com>
2023-10-23 22:59:50 +0900

    feat: :sparkles: add auth&users service, controller, module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, cache: true })], //전역 모듈로 설정
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [ConfigModule.forRoot({ isGlobal: true, cache: true }), UsersModule, AuthModule], //전역 모듈로 설정
+  controllers: [AppController, UsersController],
+  providers: [AppService, UsersService],
 })
 export class AppModule {}
