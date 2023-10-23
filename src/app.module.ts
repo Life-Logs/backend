12f2053ab72c5b06a227d25e3d2d551d71e7e371
Intra id : tkim <39645477+kth2624@users.noreply.github.com>
2023-10-23 22:41:01 +0900

    feat: add config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true, cache: true })], //전역 모듈로 설정
   controllers: [AppController],
   providers: [AppService],
 })
