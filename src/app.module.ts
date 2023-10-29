81eaf18fe147d14411e60ade3ec3d32ff049e580
Intra id : tkim <39645477+kth2624@users.noreply.github.com>
2023-10-30 01:13:28 +0900

    feat: add global validationpipe

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { UsersController } from './users/users.controller';
-import { UsersService } from './users/users.service';
-import { UsersModule } from './users/users.module';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import * as path from 'path';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, cache: true }), UsersModule, AuthModule], //전역 모듈로 설정
-  controllers: [AppController, UsersController],
-  providers: [AppService, UsersService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, cache: true }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.MYSQL_HOST,
+      port: +process.env.MYSQL_PORT,
+      username: process.env.MYSQL_USERNAME,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
+      entities: [path.join(__dirname, '/entities/**.entity{.ts,.js}')],
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+    AuthModule,
+  ], //전역 모듈로 설정
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe()); //전역 파이프 설정
   const configService = app.get(ConfigService);
   await app.listen(configService.get('SERVER_PORT'));
 }
