8b42c675f7a2131b6159c36303f8287bdd188e01
Intra id : tkim <39645477+kth2624@users.noreply.github.com>
2023-11-19 23:41:07 +0900

    feat: add routine api

--- src/routine/routine.controller.ts
+++ src/routine/routine.controller.ts
@@ -1,4 +1,60 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Get,
+  Post,
+  Param,
+  Put,
+  Delete,
+} from '@nestjs/common';
+import {
+  ApiTags,
+  ApiBody,
+  ApiOkResponse,
+  ApiParam,
+  ApiOperation,
+} from '@nestjs/swagger';
+import { RoutineService } from './routine.service';
+import { Routine } from 'src/entities/routine.entity';
 
+@ApiTags('routine')
 @Controller('routine')
-export class RoutineController {}
+export class RoutineController {
+  constructor(private readonly routineService: RoutineService) {}
+
+  @Post()
+  @ApiOperation({ summary: '루틴 생성' })
+  @ApiOkResponse({ description: '루틴 생성 성공', type: Routine })
+  createRoutine(@Body() routine: Routine) {
+    return this.routineService.createRoutine(routine);
+  }
+
+  @Get()
+  @ApiOperation({ summary: '모든 루틴 조회' })
+  @ApiOkResponse({ description: '모든 루틴 조회 성공', type: [Routine] })
+  getAllRoutines() {
+    return this.routineService.getAllRoutines();
+  }
+
+  @Get('/:id')
+  @ApiOperation({ summary: '루틴 조회' })
+  @ApiParam({ name: 'id', required: true })
+  getRoutine(@Param('id') id: number) {
+    return this.routineService.getRoutine(id);
+  }
+
+  @Put('/:id')
+  @ApiOperation({ summary: '루틴 업데이트' })
+  @ApiParam({ name: 'id', required: true })
+  @ApiOkResponse({ description: '루틴 업데이트 성공', type: Routine })
+  updateRoutine(@Param('id') id: number, @Body() routine: Routine) {
+    return this.routineService.updateRoutine(id, routine);
+  }
+
+  @Delete('/:id')
+  @ApiOperation({ summary: '루틴 삭제' })
+  @ApiParam({ name: 'id', required: true })
+  deleteRoutine(@Param('id') id: number) {
+    return this.routineService.deleteRoutine(id);
+  }
+}
--- src/routine/routine.service.ts
+++ src/routine/routine.service.ts
@@ -1,4 +1,33 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Routine } from 'src/entities/routine.entity';
 
 @Injectable()
-export class RoutineService {}
+export class RoutineService {
+  constructor(
+    @InjectRepository(Routine)
+    private readonly routineRepository: Repository<Routine>,
+  ) {}
+
+  async createRoutine(routine: Routine): Promise<Routine> {
+    return this.routineRepository.save(routine);
+  }
+
+  async getAllRoutines(): Promise<Routine[]> {
+    return this.routineRepository.find();
+  }
+
+  async getRoutine(id: number): Promise<Routine> {
+    return this.routineRepository.findOne(id);
+  }
+
+  async updateRoutine(id: number, routine: Routine): Promise<Routine> {
+    await this.routineRepository.update(id, routine);
+    return this.getRoutine(id);
+  }
+
+  async deleteRoutine(id: number): Promise<void> {
+    await this.routineRepository.delete(id);
+  }
+}
