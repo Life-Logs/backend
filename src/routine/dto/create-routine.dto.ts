4fca75cb1480c99e91805d20cc4ceb03f974d7a8
Intra id : tkim <39645477+kth2624@users.noreply.github.com>
2023-11-24 23:04:38 +0900

    refactor: create-routine dto 주석 추가

--- src/routine/dto/create-routine.dto.ts
+++ src/routine/dto/create-routine.dto.ts
@@ -18,9 +18,33 @@ export class CreateRoutineDto {
   @IsOptional()
   @IsString()
   type?: string;
+  //카운트, 퍼센트, 체크박스
 
   @IsObject()
   datetime: object;
+  //json으로
+  //{
+  //  "monday": {
+  //    "start": "09:00",
+  //    "end": "18:00"
+  //  },
+  //  "tuesday": {
+  //    "start": "09:00",
+  //    "end": "18:00"
+  //  },
+  //  "wednesday": {
+  //    "start": "09:00",
+  //    "end": "18:00"
+  //  },
+  //  "thursday": {
+  //    "start": "09:00",
+  //    "end": "18:00"
+  //  },
+  //  "friday": {
+  //    "start": "09:00",
+  //    "end": "18:00"
+  //  }
+  //}
 
   @IsOptional()
   @IsBoolean()
@@ -33,6 +57,7 @@ export class CreateRoutineDto {
   @IsOptional()
   @IsArray()
   routineTags?: string[];
+  //비워두면 루틴이름으로 태그 생성
 
   @IsDateString()
   activedAt: Date;
